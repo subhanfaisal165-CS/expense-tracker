395a27b313193cf0e5143c5a549e060718a262e9
Subhan Faisal <subhanfaisal165@gmail.com>
2026-08-10 03:42:30 +0500

    Add Expense Feature

---
+++ expense.py
@@ -0,0 +1,5 @@
+def add_expense():
+    name = input("Enter expense name: ")
+    amount = float(input("Enter amount: "))
+
+    print(f"Expense added: {name} - Rs. {amount}")
---
+++ main.py
@@ -0,0 +1,5 @@
+from expense import add_expense
+
+print("=== Expense Tracker ===")
+
+add_expense()
