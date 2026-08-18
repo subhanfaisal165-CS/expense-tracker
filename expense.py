0d820983f5af08e137b4ed514de75cd33fd46ddf
Subhan Faisal <subhanfaisal165@gmail.com>
2026-08-19 02:26:24 +0500

    Complete expense tracker features

--- expense.py
+++ expense.py
@@ -1,8 +1,62 @@
+from file_handler import load_expenses, save_expenses
+
+
 def add_expense():
     name = input("Enter expense name: ")
-    amount = float(input("Enter amount: "))
+
+    try:
+        amount = float(input("Enter amount: "))
+    except ValueError:
+        print("Please enter a valid number.")
+        return
+
+    if amount <= 0:
+        print("Amount must be greater than 0.")
+        return
+
+    expense = {
+        "name": name,
+        "amount": amount
+    }
+
+    expenses = load_expenses()
+    expenses.append(expense)
+    save_expenses(expenses)
 
     print(f"Expense added: {name} - Rs. {amount}")
+    print("Expense saved successfully!")
 
 
-print("Expense added successfully!")
+def view_expenses():
+    expenses = load_expenses()
+
+    for i, expense in enumerate(expenses, start=1):
+        print(f"{i}. {expense['name']} - Rs. {expense['amount']}")
+
+
+def total_expenses():
+    expenses = load_expenses()
+
+    total = 0
+
+    for expense in expenses:
+        total += expense["amount"]
+
+    print(f"Total Expenses: Rs. {total}")
+
+
+def delete_expense():
+    expenses = load_expenses()
+
+    if not expenses:
+        print("No expenses found.")
+        return
+
+    view_expenses()
+
+    choice = int(input("Enter expense number to delete: "))
+
+    deleted = expenses.pop(choice - 1)
+    save_expenses(expenses)
+
+    print(f"{deleted['name']} deleted successfully!")
--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-from expense import add_expense
-
+from expense import add_expense, view_expenses, total_expenses, delete_expense
 print("=== Expense Tracker ===")
 
-add_expense()
+while True:
+    print("\n1. Add Expense")
+    print("2. View Expenses")
+    print("3. Total Expense")
+    print("4. Delete Expense")
+    print("5. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        add_expense()
+
+    elif choice == "2":
+        view_expenses()
+
+    elif choice == "3":
+        total_expenses()
+
+    elif choice == "4":
+        delete_expense()
+
+    elif choice == "5":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
